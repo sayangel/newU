09a90e55900aaa068f5f9bb62d6ab2563f820412
angel <sayangel@gmail.com>
2014-06-14 00:28:15 -0400

    more updates

--- traits.js
+++ traits.js
@@ -2,12 +2,4 @@ function getTraits() {
   return {};
 }
 
-function personalities() {
-  return ['confident'];
-}
-
-function archetypes() {
-  return ['hacker'];
-}
-
 exports.getTraits = getTraits;
