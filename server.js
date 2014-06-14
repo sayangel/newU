f439f6bff9047adfe9e68d92d16d6b686613670b
angel <sayangel@gmail.com>
2014-06-13 22:25:35 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.static(__dirname));
+//app.engine('html', require('ejs').renderFile);
+
+var server = require('http').Server(app);
+
+app.get('/', function(req, res) {
+  //res.render('index.ejs');
+  res.send('Starting...')
+});
+
+app.use(function(req, res, next){
+  res.send(404, '<h1>This is probably not the page you are looking for. </br><a href="/">Take me back</a></h1>');
+})
+
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
