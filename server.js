09eb237933c45ba85864f0fd1c7196feca477366
angel <sayangel@gmail.com>
2014-06-14 12:11:34 -0400

    commited with an error. woops.

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/', function(req, res){
         if(!err)
         {
           res.send("Thanks for submitting!");
-          var messageTxt = "Remember, " + userTraits.nickname + ", don't be afraid to not be yourself! Open with: \n\n" + userTraits.firstLiner "\n\nThen maybe start a discussion with: \n\n" + userTraits.discussion + "\n\nWe're here to help if you need anything else!";
+          var messageTxt = "Remember, " + userTraits.nickname + ", don't be afraid to not be yourself! Open with: \n\n" + userTraits.firstLiner + "\n\nThen maybe start a discussion with: \n\n" + userTraits.discussion + "\n\nWe're here to help if you need anything else!";
           console.log(messageTxt);
 
           client.messages.create({
