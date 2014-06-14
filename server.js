66072006e379b727c99b4268696854958fe4d7ec
angel <sayangel@gmail.com>
2014-06-14 10:29:35 -0400

    adding text functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var twilio = require('twilio');
 var traits = require('./profile.js');
+var reddit = require('redwrap');
 
 var app = express();
 app.use(express.logger());
@@ -50,32 +51,71 @@ app.get('/', function(req, res) {
   //res.render('index.ejs');
   var num1 = Math.floor(Math.random() * 12);
   var num2 = Math.floor(Math.random() * 12);
-  console.log("RANDOS");
-  console.log(num1);
-  console.log(num2);
   var profile = traits.getProfile(personalities[num1], archetypes[num2]);
 
-  client.messages.create({
-	to: "3235135285",
-	from: "+19177465463",
-	body: "hellooooo this is the newU",
-  }, function(err, message) {
-  	console.log(message.sid);
+  res.render('index.ejs', {data: profile});
+});
+
+app.get('/apitest', function(req, res){
+  var topics = "";
+
+  reddit.r('worldnews', function(err, data, res){
+    for(var i = 0; i < 5; i++){
+      if(!data.data.children[i].data.is_self){
+        //console.log(data.data.children[i].data.title); //outputs object representing first page of WTF subreddit
+        topics += data.data.children[i].data.title + "\n\n";
+      }
+    }
+    var messageTxt = "If you don't know what to talk about, here are some topics for discussion: \n\n" + topics;
+    console.log(messageTxt);
+
+    client.messages.create({
+    to: "3235135285",
+    from: "+19177465463",
+    body: messageTxt,
+    }, function(err, message) {
+      console.log(message.sid);
+    });
+
   });
 
-  res.render('index.ejs', {data: profile});
+
+
 });
 
 app.post('/sms', twilio.webhook('fc40126ed4df188851c6061be60b110c', { host:'newu.herokuapp.com', protocol:'https' }), function(req, res){
  console.log("%s: %s", req.body.From, req.body.Body);
 
- client.messages.create({
-  	to: "3235135285",
-  	from: "+19177465463",
-  	body: "thx",
-  }, function(err, message) {
-  	console.log(message.sid);
-  });
+ if(req.body.Body === "joke")
+ {
+   reddit.r('jokes', function(err, data, res){
+
+    var post = Math.floor(Math.random() * 5);
+     var joke = data.data.children[post].data.title + "\n" + data.data.children[post].data.selftext;
+
+     var messageTxt = "This one will knock 'em dead: \n\n" + joke;
+     console.log(messageTxt);
+
+     client.messages.create({
+     to: "3235135285",
+     from: "+19177465463",
+     body: messageTxt,
+     }, function(err, message) {
+       console.log(message.sid);
+     });
+
+   });
+
+ }
+ else{
+   client.messages.create({
+    	to: "3235135285",
+    	from: "+19177465463",
+    	body: "thx",
+    }, function(err, message) {
+    	console.log(message.sid);
+    });
+  }
 });
 
 app.use(function(req, res, next){
